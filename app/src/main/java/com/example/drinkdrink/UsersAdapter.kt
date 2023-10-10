36b6775de4635604525abd059dc1df4617f5c0af
Max Metyolkin <48799140+mmetyolkin@users.noreply.github.com>
2023-10-10 21:40:27 +0300

    new

--- app/src/main/java/com/example/drinkdrink/UsersAdapter.kt
+++ app/src/main/java/com/example/drinkdrink/UsersAdapter.kt
@@ -1,17 +1,11 @@
 package com.example.drinkdrink
 
-import android.app.Dialog
-import android.content.DialogInterface
-import android.os.Bundle
+
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
-import android.view.Window
-import android.widget.Button
 import android.widget.PopupMenu
-import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.drinkdrink.databinding.ItemUserBinding
